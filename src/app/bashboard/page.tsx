ddc257d396e8d5728bb8e259c13c37c2f30ac9d6
NguyenHuuNam3 <nhnam4411@gmail.com>
2026-04-09 01:23:55 +0700

    fix build

--- src/app/bashboard/page.tsx
+++ src/app/bashboard/page.tsx
@@ -11,17 +11,17 @@ export default function Dashboard() {
   const { fetchUsers, users, user } = useUser()
 
   useEffect(() => {
-    if (!user || !user?.classId === 'admin333' && user.admin) {
+    if (!user || !(user?.classId === 'admin333') && user.admin) {
       router.push('/')
     }
 
   }, [user, router])
 
   return (
-      <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-        <Card className="border-0 shadow-xl">
-          <ScoresTable />
-        </Card>
-      </div>
+    <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+      <Card className="border-0 shadow-xl">
+        <ScoresTable />
+      </Card>
+    </div>
   )
 }
